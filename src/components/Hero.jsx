8e934b652ff33cfc133449ea4535a4df894b280c
Jaime Sanchez <jaime.sanchezz@estudiantes.uv.cl>
2026-05-23 23:17:16 -0400

    Agregar sección Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import './Hero.css';
+
+
+function Hero() {
+    return(
+        <section id='inicio'className="hero">
+            <div className="hero-overlay"></div>
+            <div className="hero-content">
+                <h1>Restobar Brasa</h1>
+                <p>Tu punto de encuentro en Rinconada. Disfruta de nuestras increibles burguers, coctelería y el mejor ambiente</p>
+            </div>
+        </section>
+    );
+}
+
+export default Hero;
